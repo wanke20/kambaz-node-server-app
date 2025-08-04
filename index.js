04b1fcfeb85105415a614e4c6682941cb07df1a0
wanke20 <122842952+wanke20@users.noreply.github.com>
2025-08-04 19:44:42 -0400

    change index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV !== "development") {
     sessionOptions.cookie = {
         sameSite: "none",
         secure: true,
-        domain: process.env.NODE_SERVER_DOMAIN,
+        // domain: process.env.NODE_SERVER_DOMAIN,
     };
 }
 app.use(session(sessionOptions));
