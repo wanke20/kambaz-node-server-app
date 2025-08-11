161d5d3cd79ea7b883319f0fee1aa71ffb97c7a3
wanke20 <122842952+wanke20@users.noreply.github.com>
2025-08-11 15:23:18 -0400

    fix env file

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -8,7 +8,7 @@ export const createUser = (user) => {
 export const findAllUsers = () => model.find();
 export const findUserById = (userId) => model.findById(userId);
 export const findUserByUsername = (username) =>  model.findOne({ username: username });
-export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
+export const findUserByCredentials = (username, password) => model.findOne({ username, password });
 export const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role })
 export const findUsersByPartialName = (partialName) => {
   const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
