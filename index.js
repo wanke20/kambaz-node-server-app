189c59d6e67ffa4d2b8bc4088b42c95bed4e5bdd
wanke20 <122842952+wanke20@users.noreply.github.com>
2025-08-11 22:46:17 -0400

    update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const allowedOrigins = [
 ];
 
 const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
-await mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {
   tlsAllowInvalidCertificates: true
 });
 const app = express()
